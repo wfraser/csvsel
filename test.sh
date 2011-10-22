c1d53a5b3bc987c5e78ba3d4b5cdb92715c353a2
William R. Fraser <wfraser@galileo.home>
2011-10-22 02:50:42 -0700

    include test results

--- test.sh
+++ test.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
-./csvsel "select %3 where %2 = %3 or (%1 = Wyoming or %1 = Washington)" < test.csv
+./csvsel "select %3 where %2 = %3 or (%1 = Illinois or %1 = Washington)" < test.csv
+
+# should print:
+#
+# Little Rock
+# Denver
+# Atlanta
+# Honolulu
+# Boise
+# Chicago
+# Indianapolis
+# Des Moines
+# Boston
+# Oklahoma City
+# Providence
+# Columbia
+# Salt Lake City
+# Seattle
+# Charleston
+# Cheyenne
 
